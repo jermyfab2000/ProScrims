13796624a5dccaf9ac8aec05950e9d193a80a9ca
jermyfab2000 <jermyfab2000@gmail.com>
2019-01-15 19:22:35 -0600

    first commit

---
+++ cmds/auto-start.js
@@ -0,0 +1,123 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const settings = require('./../settings.json');
+const owner = settings.owner;
+
+
+module.exports.run = async (bot, message, args) => {
+
+    let voice = "534890071606689792";
+    let codes = "534173973953642496";
+    let general = "534190682332594187";
+	let req = "534890035208650762"
+
+    console.log("Activating Auto Comamand");
+
+    let raw = fs.readFileSync('./roles.json');
+    let allowedRoles = JSON.parse(raw);
+
+    let validation = function(serverRoles, userRoles){
+     let val = false;
+     serverRoles.forEach((role) => {
+         userRoles.forEach((usr) => {
+                if (role == usr){
+                  val = true;
+                }
+            });
+        });
+        return val;
+    }
+
+
+    let intro = new Discord.RichEmbed()
+        .setTitle("Las Scrims Inician Automaticamente Cada Media Hora, 24/7")
+        .setColor("#00cc00");
+
+        bot.guilds.get(message.guild.id).channels.get(general).send({embed: intro}).catch((err) => {
+            console.log(err);
+        });
+
+        let autoScrims = setInterval(() => {
+            let time = new Date();
+            let min = time.getMinutes();
+            let embed = new Discord.RichEmbed()
+                .setColor("#00cc00");
+
+            if (min === 50 || min === 20){
+				embed.setTitle("Siguiente Scrim En 10 Minutos **Unanse al Conteo!!!**");
+                bot.guilds.get(message.guild.id).channels.get(general).send({embed: embed}).catch((err) => {
+                    console.log(err);
+                });
+            }else if (min === 55 || min === 25){
+                embed.setTitle("Siguiente Scrim En 5 Minutos **Unanse al Conteo!!!**");
+                bot.guilds.get(message.guild.id).channels.get(general).send({embed: embed}).catch((err) => {
+                    console.log(err);
+                });
+            }else if (min === 59 || min === 29){
+                embed.setTitle("Siguiente Scrim En 1 Minuto **Por Favor estar atento al Conteo y cargar contenido!");
+                bot.guilds.get(message.guild.id).channels.get(general).send({embed: embed}).catch((err) => {
+                    console.log(err);
+                });
+            }else if (min === 00 || min === 30){
+                embed.setTitle("La Scrim Ya Empezo, Buena Suerte!! __**Recuerden Poner Sus Ultimos 3 digitos En #código**__");
+                bot.guilds.get(message.guild.id).channels.get(general).send({embed: embed}).catch((err) => {
+                    console.log(err);
+                });
+
+                bot.guilds.get(message.guild.id).channels.get(codes).send("!count").catch((err) => {
+                    console.log(err);
+                });
+
+                bot.guilds.get(message.guild.id).channels.get(codes).send("!start").catch((err) => {
+                    console.log(err);
+                });
+            }else if (min === 01 || min === 31){
+                embed.setTitle("Se realizara un REQ En 4 minutos!");
+                bot.guilds.get(message.guild.id).channels.get(general).send({embed: embed}).catch((err) => {
+                    console.log(err);
+                });
+            }else if (min === 04 || min === 34){
+                embed.setTitle("1 Minuto Para REQ Por Favor estar atento al Conteo y cargar contenido");
+                bot.guilds.get(message.guild.id).channels.get(general).send({embed: embed}).catch((err) => {
+                    console.log(err);
+                });
+			}else if (min === 05 || min === 35){
+                embed.setTitle("Iniciando REQ!");
+                bot.guilds.get(message.guild.id).channels.get(general).send({embed: embed}).catch((err) => {
+                    console.log(err);
+                });
+
+                bot.guilds.get(message.guild.id).channels.get(req).send("!count").catch((err) => {
+                    console.log(err);
+                });
+
+                bot.guilds.get(message.guild.id).channels.get(req).send("!start").catch((err) => {
+                    console.log(err);
+                });
+			
+			}
+				
+            const filter = m => !m.author.bot;
+            const collect = bot.guilds.get(message.guild.id).channels.get(general)
+                .createMessageCollector(filter, {time: 60000});
+
+            collect.on('collect', m => {
+                if (m.content === "!auto-stop"){
+                    if(validation(allowedRoles.roles, m.member.roles.array()) || m.member.id === owner){
+                        clearInterval(autoScrims);
+                        collect.stop();
+                        bot.guilds.get(message.guild.id).channels.get(general).send("Auto Scrims Detenidas").catch((err) => {
+                            console.log(err);
+                        });
+                    }
+                }
+            });
+
+
+        }, 60000);
+    
+}
+
+module.exports.help = {
+    name: "auto-start"
+}
---
+++ cmds/start.js
@@ -0,0 +1,176 @@
+const Discord = require ('discord.js');
+const Listing = require ('./../modules/Listing');
+const fs = require('fs');
+const settings = require('./../settings.json');
+const owner = settings.owner;
+
+module.exports.run = async (bot, message, args) => {
+   let roles = message.guild.roles;
+   let scrimmers = message.guild.defaultRole;
+   let snipeChannel = message.channel;
+   const filter = m => !m.author.bot;
+   let game = new Listing();
+
+   let raw = fs.readFileSync('./roles.json');
+   let allowedRoles = JSON.parse(raw);
+
+    let validation = function(serverRoles, userRoles){
+        let val = false;
+        serverRoles.forEach((role) => {
+            userRoles.forEach((usr) => {
+                if (role == usr){
+                    val = true;
+                }
+            });
+        });
+        return val;
+    }
+
+   let editLast3 = null;
+
+   let startMessage = new Discord.RichEmbed()
+        .setAuthor("PRO SCRIMS", "https://i.imgur.com/hxHm6ZV.jpg")
+        .setThumbnail("https://i.imgur.com/eR5D1t0.png")
+        .setURL("https://www.twitch.tv/becc4")
+        .setDescription(`__**Scrims Multiplataforma**__
+Servidor: **BRAZIL**
+Instrucciones: 
+-Carga contenido. 
+-Espera en el canal de conteo. 
+-Al escuchar el conteo dale listo al __**GO**__. 
+-En caso de ps4 darle listo con el mouse.
+-Ingresa los últimos tres dígitos de tu servidor en __**#código.**__`)
+        .setImage("https://i.imgur.com/hxHm6ZV.jpg")
+        .setColor("#8600b3")
+        .setFooter("Dev By !Fabian Araya (Xccursed_CR)", "https://i.imgur.com/ADnSULk.jpg")
+        
+    
+    message.channel.send({embed: startMessage});    
+    
+    let time = 30;
+    let editTime = "";
+
+    let timeEmbed = new Discord.RichEmbed()
+        .setTitle("**Siguiente Partida En...**")
+        .setDescription(time + "Minutos")
+        .setColor("#8600b3");
+        
+
+    setTimeout(async () => {
+        editTime = await message.channel.send({embed: timeEmbed}).catch( (err) => {
+            console.log("No puedes editar el Codigo");
+        });
+    }, 10);    
+
+    let timeInterval = setInterval (() => {
+        if (time ===1){
+            time -=1;
+            timeEmbed.setDescription(time + "Minutos");
+            clearInterval(timeEmbed);
+        }else {
+            time -=1;
+            timeEmbed.setDescription(time + "Minutos");
+        }
+
+        editTime.edit({embed: timeEmbed}).catch((err) =>{
+            console.log("No puedes editar");
+            clearInterval(timeInterval);
+        })
+    },60000);
+
+    let last3 = new Discord.RichEmbed()
+        .setTitle ("**SERVIDORES**")
+        .setColor ("#8600b3")
+
+    setTimeout(async () => {
+        editLast3= await message.channel.send({embed: last3});
+    }, 10);
+    
+    const collector =snipeChannel.createMessageCollector(filter, {time: 180000});
+	snipeChannel.overwritePermissions(
+        scrimmers,
+        { "SEND_MESSAGES": true}
+    )
+
+    collector.on('collect', m => {
+
+        console.log(`Collected ${m.content} | ${m.author}`);       
+        
+        if (validation(allowedRoles.roles,m.member.roles.array()) || m.member.id === owner){
+            if (m.content === "!start" || m.content === "!stop"){
+                collector.stop();
+                console.log("Collector Stoped");
+                return;
+            }
+        }   
+        if (game.data.length ===0 && m.content.length === 3){
+            game.addID(m.content.toUpperCase(), m.author);
+        }else if (m.content.length === 3){
+            if (game.userPresent(m.author)){
+                game.deleteUserEntry(m.author);
+                if (game.idPresent(m.content.toUpperCase())){
+                    game.addUser(m.content.toUpperCase(), m.author);
+                }else {
+                     game.addID(m.content.toUpperCase(), m.author);
+                }
+            } else {
+                if (game.idPresent(m.content.toUpperCase())){
+                    game.addUser(m.content.toUpperCase(), m.author);
+                }else {
+                    game.addID(m.content.toUpperCase(), m.author);
+                }
+            }
+        }
+
+    game.sort();
+
+    let str = "";
+    last3 = new Discord.RichEmbed()
+        .setTitle("**SERVIDORES**")
+        .setColor("#8600b3")
+
+    for (var i =0; i < game.data.length; i++){
+        str = "";
+        for (var j = 0; j < game.data[i].users.length ; j++){
+            str += game.data[i].users[j] + "\n";
+        }
+        last3.addField(`${game.data[i].id.toUpperCase()} - ${game.data[i].users.length} Jugadores` , str, true);
+    }    
+
+    editLast3.edit({embed: last3}).catch((err) => {
+        console.log("error no puedes editar");
+    });
+
+    if (m.deletable){
+        m.delete().catch((err) => {
+            console.log("No puedes Borrar");
+            console.log(err);
+        });
+    }
+
+    });
+
+    collector.on('end', collected => {
+
+        console.log(`Collected ${collected.size} items`);
+        let endMessage = new Discord.RichEmbed()
+			.setAuthor("Pro Scrims", "https://i.imgur.com/hxHm6ZV.jpg")
+			.setColor("0x00AE86")
+            .setDescription("**No Se Aceptan Mas Codigos En Este Punto Buena Suerte!**")
+            .setFooter("Chat Bloqueado" , "https://i.imgur.com/iGo7XPH.png")
+		message.channel.send({embed: endMessage});
+		snipeChannel.overwritePermissions(
+            scrimmers,
+            { "SEND_MESSAGES": false}
+        );
+    });
+}
+
+
+
+
+
+
+module.exports.help = {
+    name: "start"
+}
