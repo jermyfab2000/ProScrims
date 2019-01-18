b1d17f47b1ba5c8d115d135decbfdf6664e3216d
jermyfab2000 <jermyfab2000@gmail.com>
2019-01-18 02:56:19 -0600

    Update start.js

--- cmds/start.js
+++ cmds/start.js
@@ -30,20 +30,21 @@ module.exports.run = async (bot, message, args) => {
 
    let startMessage = new Discord.RichEmbed()
         .setAuthor("PRO SCRIMS", "https://i.imgur.com/hxHm6ZV.jpg")
+        .setTitle("__**Scrims Multiplataforma**__")
         .setThumbnail("https://i.imgur.com/eR5D1t0.png")
         .setURL("https://www.twitch.tv/becc4")
-        .setDescription(`__**Scrims Multiplataforma**__
-Servidor: **BRAZIL**
-Instrucciones: 
--Carga contenido. 
--Espera en el canal de conteo. 
--Al escuchar el conteo dale listo al __**GO**__. 
--En caso de ps4 darle listo con el mouse.
--Ingresa los últimos tres dígitos de tu servidor en __**#código.**__`)
+        .addField("Instrucciones :",`
+		-Servidor: __**US-East**__
+        -Carga contenido. 
+        -Espera en el canal de conteo. 
+        -Al escuchar el conteo dale listo al __**GO**__. 
+        -En caso de ps4 darle listo con el mouse.
+        -Ingresa los últimos tres dígitos de tu servidor`)
         .setImage("https://i.imgur.com/IeIuzix.png")
-        .setColor("#8600b3")
+        .setColor("#24E711")
         .addField("Hosted by" , message.author)
         .setFooter("Dev By !Fabian Araya (Xccursed_CR)", "https://i.imgur.com/ADnSULk.jpg")
+	.setTimestamp()
         
     
     message.channel.send({embed: startMessage});    
@@ -54,7 +55,7 @@ Instrucciones:
     let timeEmbed = new Discord.RichEmbed()
         .setTitle("**Siguiente Partida En...**")
         .setDescription(time + "Minutos")
-        .setColor("#8600b3");
+        .setColor("#EDFF21");
         
 
     setTimeout(async () => {
@@ -63,24 +64,25 @@ Instrucciones:
         });
     }, 10);    
 
-        let timeInterval = setInterval(()=> {
-        if (time >= 2){
-            time -= 1;
-            timeEmbed.setDescription(time + " Minutos");
-        }else if (time === 1){
-            time -= 1;
-            timeEmbed.setDescription(time + " Minutos");
-            clearInterval(timeInterval);
+    let timeInterval = setInterval (() => {
+        if (time ===1){
+            time -=1;
+            timeEmbed.setDescription(time + "Minutos");
+            clearInterval(timeEmbed);
+        }else {
+            time -=1;
+            timeEmbed.setDescription(time + "Minutos");
         }
-        editTime.edit({embed: timeEmbed}).catch((err) => {
-            console.log("Cant edit timer, clearing interval");
+
+        editTime.edit({embed: timeEmbed}).catch((err) =>{
+            console.log("No puedes editar");
             clearInterval(timeInterval);
-        });
+        })
     },60000);
 
     let last3 = new Discord.RichEmbed()
-        .setTitle ("**SERVIDORES**")
-        .setColor ("#8600b3")
+        .setTitle ("**Servidores**")
+        .setColor ("#1E2460")
 
     setTimeout(async () => {
         editLast3= await message.channel.send({embed: last3});
@@ -126,8 +128,8 @@ Instrucciones:
 
     let str = "";
     last3 = new Discord.RichEmbed()
-        .setTitle("**SERVIDORES**")
-        .setColor("#8600b3")
+        .setTitle("**Servidores**")
+        .setColor("#1E2460")
 
     for (var i =0; i < game.data.length; i++){
         str = "";
@@ -154,8 +156,7 @@ Instrucciones:
 
         console.log(`Collected ${collected.size} items`);
         let endMessage = new Discord.RichEmbed()
-			.setAuthor("Pro Scrims", "https://i.imgur.com/hxHm6ZV.jpg")
-			.setColor("0x00AE86")
+			.setColor("#F80000")
             .setDescription("**No Se Aceptan Mas Codigos En Este Punto Buena Suerte!**")
             .setFooter("Chat Bloqueado" , "https://i.imgur.com/iGo7XPH.png")
 		message.channel.send({embed: endMessage});
@@ -174,3 +175,4 @@ Instrucciones:
 module.exports.help = {
     name: "start"
 }
+
